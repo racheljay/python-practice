2a62db2f79f0db1b3e1668bdfc4a3d1a439faa3d
Rachel <racheljay42@gmail.com>
2021-02-08 15:53:21 -0500

    added lives

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -17,7 +17,6 @@ def get_valid_word(words):
 # they are unordered and un-indexed
 # so... an object basically?
 
-
 def hangman():
     word = get_valid_word(words)
     # letters in the word to keep track of what's been guessed
@@ -25,11 +24,13 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # starts empty to store what the user has guessed
 
+    lives = 6
+
     # getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
         # what current word guess is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -41,12 +42,20 @@ def hangman():
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
 
+            else:
+                lives = lives - 1 #takes away a life if wrong guess
+                print('Letter is not in this word')
+
         elif user_letter in used_letters:
             print('You have already used that character. Please try again')
 
         else:
             print('This is not a valid character.')
-    # gets here when let(word_letters) == 0
+    # gets here when let(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print('You have died, sorry. The word was', word)
+    else:
+        print('You have guess the word', word, '!!')
 
 hangman()
 
