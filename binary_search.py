8df0615d48647ccd5107fd48e21b7fcbee7ec507
Rachel <racheljay42@gmail.com>
2021-03-11 16:48:38 -0500

    prints out time difference sorting 10000 length list

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,5 @@
+import random
+import time
 # Implementation of binary search algorithm
 
 # Proving that binary search is faster than naive search
@@ -27,7 +29,7 @@ def binary_search(l, target, low=None, high=None):
     if high < low:
         return -1
     # example l = [1,3,5,10,12] should return 3
-    # 2 -- // means how many times will this go into length
+    # 2 -- // means how many times will this go into length or divide and round down
     midpoint = (low + high) // 2
 
     if l[midpoint] == target:
@@ -40,7 +42,26 @@ def binary_search(l, target, low=None, high=None):
 
 
 if __name__ == '__main__':
-    l = [1, 3, 5, 10, 12]
-    target = 5
-    print(naive_search(l, target))
-    print(binary_search(l, target))
+    # l = [1, 3, 5, 10, 12]
+    # target = 5
+    # print(naive_search(l, target))
+    # print(binary_search(l, target))
+
+    length = 10000
+    # build a sorted list of length 10,000
+    sorted_list = set()
+    while len(sorted_list) < length:
+        sorted_list.add(random.randint(-3*length, 3*length)) # gives us a range of numbers from -30,000 to 30,000
+    sorted_list = sorted(list(sorted_list))
+
+    start = time.time()
+    for target in sorted_list:
+        naive_search(sorted_list, target)
+    end = time.time()
+    print("Naive search time: ", (end - start))
+
+    start = time.time()
+    for target in sorted_list:
+        binary_search(sorted_list, target)
+    end = time.time()
+    print("Binary search time: ", (end - start))
